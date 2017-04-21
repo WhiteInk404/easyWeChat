5e641dd511c2ca715b7d4e89f700ae44da35775a
WhiteInk404 <johnnypeibai@gmail.com>
2017-04-21 09:41:55 +0800

    add CSRF and use Log

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
-//use Log;
+use Log;
 
 class WechatController extends Controller
 {
@@ -17,7 +17,7 @@ class WechatController extends Controller
      */
     public function serve()
     {
-//        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $wechat = app('wechat');
         $wechat->server->setMessageHandler(function($message){
@@ -53,7 +53,7 @@ class WechatController extends Controller
 
         });
 
-//        Log::info('return response.');
+        Log::info('return response.');
 
         return $wechat->server->serve();
     }
