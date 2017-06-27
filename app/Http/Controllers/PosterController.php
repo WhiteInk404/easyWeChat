5c251776c1f26a36c58fa3cf221b24d13f7acb37
WhiteInk404 <johnnypeibai@gmail.com>
2017-06-27 17:13:21 +0800

    optimizing thumb qrcode function

--- app/Http/Controllers/PosterController.php
+++ app/Http/Controllers/PosterController.php
@@ -30,6 +30,32 @@ class PosterController extends Controller
         return $headimgurl;
     }
 
+    public function thumb($filename,$width,$height)
+    {
+        //获取原图像$filename的宽度$width_orig和高度$height_orig
+        list($width_orig,$height_orig) = getimagesize($filename);
+        //根据参数$width和$height值，换算出等比例缩放的高度和宽度
+        if ($width && ($width_orig<$height_orig)){
+            $width = ($height/$height_orig)*$width_orig;
+        }else{
+            $height = ($width / $width_orig)*$height_orig;
+        }
+
+        //将原图缩放到这个新创建的图片资源中
+        $image_p = imagecreatetruecolor($width, $height);
+        //获取原图的图像资源
+        $image = imagecreatefromjpeg($filename);
+
+        //使用imagecopyresampled()函数进行缩放设置
+        imagecopyresampled($image_p,$image,0,0,0,0,$width,$height,$width_orig,$height_orig);
+
+        //将缩放后的图片$image_p保存，100(质量最佳，文件最大)
+        imagejpeg($image_p,$filename);
+
+        imagedestroy($image_p);
+        imagedestroy($image);
+    }
+    
     public function getqrcode($openId)
     {
         $qrcodeinfo = $this->wechat->qrcode->forever(56);
@@ -40,23 +66,23 @@ class PosterController extends Controller
 //        var_dump($qrcodeurl);
 //        var_dump($ticket);
 //        var_dump($url);
-
-        /**
-         * resimple qrcode image to 300*300
-         *
-         */
-        header ('Content-Type: image/png');
-        $qrcode_source = imagecreatefromjpeg($qrcodeurl);
-        $qrcode_thumb = imagecreatetruecolor(300, 300);
-        imagecopyresampled($qrcode_thumb, $qrcode_source, 0, 0, 0, 0, 300, 300, 430, 430);
-
-        /**
-         * add headimage to qrcode
-         */
-        $head_source = imagecreatefromjpeg($this->person($openId));
-        imagecopy($qrcode_thumb, $head_source, 118, 118, 0, 0, 64, 64);
-
-        imagejpeg($qrcode_thumb);
+        $this->thumb($qrcodeurl,300,300);
+//        /**
+//         * resimple qrcode image to 300*300
+//         *
+//         */
+//        header ('Content-Type: image/png');
+//        $qrcode_source = imagecreatefromjpeg($qrcodeurl);
+//        $qrcode_thumb = imagecreatetruecolor(300, 300);
+//        imagecopyresampled($qrcode_thumb, $qrcode_source, 0, 0, 0, 0, 300, 300, 430, 430);
+//
+//        /**
+//         * add headimage to qrcode
+//         */
+//        $head_source = imagecreatefromjpeg($this->person($openId));
+//        imagecopy($qrcode_thumb, $head_source, 118, 118, 0, 0, 64, 64);
+//
+//        imagejpeg($qrcode_thumb);
 
     }
 
