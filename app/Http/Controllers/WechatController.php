2e43e0f133c5b45bcf53ee703c51b05288b2afbe
WhiteInk404 <johnnypeibai@gmail.com>
2017-07-31 12:58:09 +0800

    add News message to Menu

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -34,17 +34,15 @@ class WechatController extends Controller
                     elseif ($message->Event == 'VIEW')
                         return '';
                     elseif ($message->Event == 'CLICK' && $message->EventKey == 'ywcdt')
-
                         return new Material('mpnews', 'wnbcD3E9njenEUjhH9A05wplfnnECwQQhOSmnn79hXs');
-
                     else
                         return '';
                     break;
                 case 'text':
                     if ($message->Content == '7000')
                         return "链接: https://pan.baidu.com/s/1jIl4nMu 密码: xfqf";
-                    else if ($message->Content == '海报')
-                        return "海报";
+                    else if ($message->Content == 'ywcdt')
+                        return new Material('mpnews', 'wnbcD3E9njenEUjhH9A05wplfnnECwQQhOSmnn79hXs');
                     else
                         return "你好，".$userApi->get($message->FromUserName)->nickname."。\n如果您需要客服帮助，请添加微信号：xuechun_1991";
                     break;
