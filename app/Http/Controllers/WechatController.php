4d16a4042f487b459e0842ac5124414da023e05a
WhiteInk404 <johnnypeibai@gmail.com>
2017-07-31 16:18:28 +0800

    generate QRCODE

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -33,8 +33,8 @@ class WechatController extends Controller
                         if ($message->EventKey == 'qrscene_ywcdt')
                             return new News([
                                 'title'       => '英文晨读 | 慢慢来，比较快',
-                                'description' => '我们看到太阳发出的光需要8分钟，\n看到海王星反射出的光需要4个小时，\n看到银河系边缘的光至少需要2.4万年，\n看到宇宙中距离我们最远的那颗星星发出的光需要139亿年。\n所有的光芒，都需要时间才能被看到……\n\n慢慢来，比较快。\n我们一起加油！',
-                                'url'         => 'http://mp.weixin.qq.com/s?__biz=MzAxMjczOTc0OA==&mid=502809481&idx=1&sn=77b9452a1c9ec6aea42873d92e87c013&chksm=03a1d64234d65f54504126db81f3b7c9859a971ba5c8f17aaf5e7cf95c48a774884f8801f53d#rd',
+                                'description' => '我们看到太阳发出的光需要8分钟，看到海王星反射出的光需要4个小时，看到银河系边缘的光至少需要2.4万年，看到宇宙中距离我们最远的那颗星星发出的光需要139亿年。所有的光芒，都需要时间才能被看到……慢慢来，比较快。我们一起加油！',
+                                'url'         => 'http://mp.weixin.qq.com/s/76YQY7NMs45UV_Xdn51yPA',
                                 'image'       => 'http://mmbiz.qpic.cn/mmbiz_png/ceiacP5ibxGyibJdiaynFtAfmyRun0Md6C9b0l6nticmucl9WbrkJDaEibgX31hEg8rmVXJAj4mMUVKjXjXV6P7mIErA/0?wx_fmt=png'
                                 // ...
                             ]);
@@ -44,16 +44,16 @@ class WechatController extends Controller
                     elseif ($message->Event == 'SCAN' && $message->EventKey == 'ywcdt')
                         return new News([
                                 'title'       => '英文晨读 | 慢慢来，比较快',
-                                'description' => '我们看到太阳发出的光需要8分钟，\n看到海王星反射出的光需要4个小时，\n看到银河系边缘的光至少需要2.4万年，\n看到宇宙中距离我们最远的那颗星星发出的光需要139亿年。\n所有的光芒，都需要时间才能被看到……\n\n慢慢来，比较快。\n我们一起加油！',
-                                'url'         => 'http://mp.weixin.qq.com/s?__biz=MzAxMjczOTc0OA==&mid=502809481&idx=1&sn=77b9452a1c9ec6aea42873d92e87c013&chksm=03a1d64234d65f54504126db81f3b7c9859a971ba5c8f17aaf5e7cf95c48a774884f8801f53d#rd',
+                                'description' => '我们看到太阳发出的光需要8分钟，看到海王星反射出的光需要4个小时，看到银河系边缘的光至少需要2.4万年，看到宇宙中距离我们最远的那颗星星发出的光需要139亿年。所有的光芒，都需要时间才能被看到……慢慢来，比较快。我们一起加油！',
+                                'url'         => 'http://mp.weixin.qq.com/s/76YQY7NMs45UV_Xdn51yPA',
                                 'image'       => 'http://mmbiz.qpic.cn/mmbiz_png/ceiacP5ibxGyibJdiaynFtAfmyRun0Md6C9b0l6nticmucl9WbrkJDaEibgX31hEg8rmVXJAj4mMUVKjXjXV6P7mIErA/0?wx_fmt=png'
-                                // ...
+                            // ...
                             ]);
                     elseif ($message->Event == 'CLICK' && $message->EventKey == 'ywcdt')
                         return new News([
                             'title'       => '英文晨读 | 慢慢来，比较快',
-                            'description' => '我们看到太阳发出的光需要8分钟，\n看到海王星反射出的光需要4个小时，\n看到银河系边缘的光至少需要2.4万年，\n看到宇宙中距离我们最远的那颗星星发出的光需要139亿年。\n所有的光芒，都需要时间才能被看到……\n\n慢慢来，比较快。\n我们一起加油！',
-                            'url'         => 'http://mp.weixin.qq.com/s?__biz=MzAxMjczOTc0OA==&mid=502809481&idx=1&sn=77b9452a1c9ec6aea42873d92e87c013&chksm=03a1d64234d65f54504126db81f3b7c9859a971ba5c8f17aaf5e7cf95c48a774884f8801f53d#rd',
+                            'description' => '我们看到太阳发出的光需要8分钟，看到海王星反射出的光需要4个小时，看到银河系边缘的光至少需要2.4万年，看到宇宙中距离我们最远的那颗星星发出的光需要139亿年。所有的光芒，都需要时间才能被看到……慢慢来，比较快。我们一起加油！',
+                            'url'         => 'http://mp.weixin.qq.com/s/76YQY7NMs45UV_Xdn51yPA',
                             'image'       => 'http://mmbiz.qpic.cn/mmbiz_png/ceiacP5ibxGyibJdiaynFtAfmyRun0Md6C9b0l6nticmucl9WbrkJDaEibgX31hEg8rmVXJAj4mMUVKjXjXV6P7mIErA/0?wx_fmt=png'
                             // ...
                         ]);
