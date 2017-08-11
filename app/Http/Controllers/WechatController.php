f959cc1973af000de36a4ff55f81fa9849dd6264
WhiteInk404 <johnnypeibai@gmail.com>
2017-08-11 09:44:46 +0800

    edit auto-reply

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -39,7 +39,7 @@ class WechatController extends Controller
                                 // ...
                             ]);
                         else
-                            return "Hi ".$userApi->get($message->FromUserName)->nickname."\nWelcome to WeCee!\n客服微信：xuechun_1991";
+                            return "同学你好，我是Johnny。\n欢迎优秀的你来到这里，和我们一起学英语，睁眼看世界\n――――――――\n如果您需要客服帮助，请添加我的个人微信号：johnnypeibai1";
                     }
                     elseif ($message->Event == 'SCAN' && $message->EventKey == 'ywcdt')
                         return new News([
@@ -72,7 +72,7 @@ class WechatController extends Controller
                             // ...
                         ]);
                     else
-                        return "你好，".$userApi->get($message->FromUserName)->nickname."。\n如果您需要客服帮助，请添加微信号：xuechun_1991";
+                        return "你好～".$userApi->get($message->FromUserName)->nickname."\n我是Johnny，很高兴遇见你～\n加我的个人微信号（johnnypeibai1），随时撩我～";
                     break;
                 case 'image':
                     return '收到图片消息';
